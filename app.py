b048059d0daf421ae06c1265d160ded11fe20708
DanRyzer10 <angeldaniel6709@gmail.com>
2023-12-21 20:06:04 -0500

    added route solution

--- app.py
+++ app.py
@@ -6,6 +6,25 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/passenger/create')
+def create_passengers():
+    return render_template('views/passenger/create.html')
+
+@app.route('/passenger/list')
+def read_passengers():
+    return render_template('views/passenger/read.html')
+
+@app.route('/passenger/update')
+def update_passengers():
+    return render_template('views/passenger/update.html')
+
+@app.route('/passenger/delete')
+def delete_passengers():
+    return render_template('views/passenger/delete.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
